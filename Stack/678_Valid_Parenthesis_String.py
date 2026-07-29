e57f5e3374d78f0fc8acf254ebe765be77a785cf
kvajaykumar45 <kvajaykumar45@gmail.com>
2026-07-29 15:16:21 +0530

    Success

---
+++ Stack/678_Valid_Parenthesis_String.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left = []
+        star = []
+        for i in range(len(s)):
+            if s[i] == '(':
+                left.append(i)
+            elif s[i] == '*':
+                star.append(i)
+            else:
+                if left:
+                    left.pop()
+                elif star:
+                    star.pop()
+                else:
+                    return False
+        while left and star:
+            x = left.pop()
+            y = star.pop()
+            if x > y:
+                return False
+        return len(left) == 0  
